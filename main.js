4c47ad95060bf22984d9206010184885daf3e6dc
Aartipal1998 <aartipal614@gmail.com>
2024-02-08 00:10:07 +0530

    first commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let input =document.querySelector("input");
 let h1 = document.querySelector(".display-1");
 let h2 = document.querySelector(".display-2");
 let p = document.querySelector("p");
-let img = document.querySelector("#sun");
+// let img = document.querySelector('#weather');
 
 
 let getWeather = async (e) => {
@@ -13,10 +13,10 @@ let getWeather = async (e) => {
     try{
         let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1251c57f94ad433c82471238232712&q=${input.value}&aqi=yes`);
         let data = await response.json();
-        h1.innerText = `${data.current.temp_c} c`;
+        h1.innerText = `${data.current.temp_c } c`;
         h2.innerText = data.location.name;
         p.innerText = data.current.condition.text;
-        img.setAttribute("src", data.current.condition.icon);
+        img.setAttribute ("src", data.current.condition.icon);
         
     }catch{
         window.alert("invalid city name");
@@ -25,3 +25,7 @@ let getWeather = async (e) => {
 }
 
 form.addEventListener("submit", getWeather);
+
+
+
+
